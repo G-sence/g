07106cbedd43408c90cf3e55821a7131c3c50400
kingno552-admin <m0a2328468@edu.teu.ac.jp>
2024-10-07 00:55:31 +0900

    ステージ生成を実装しました

--- g2/Assets/YAMAZUMI/Script/Player_cont demo.cs
+++ g2/Assets/YAMAZUMI/Script/Player_cont demo.cs
@@ -7,10 +7,16 @@ public class Player_contdemo : MonoBehaviour
     public Vector3 pos;
     public float speed = 3.0f;
 
+    public GameObject stage1;
+    public GameObject stage2;
+    public GameObject stage3;
+    public GameObject stage4;
+    public GameObject stage5;
+    public GameObject stage6;
+
     void Start()
     {
         pos = transform.position;
-
     }
 
     // Update is called once per frame
@@ -35,5 +41,42 @@ public class Player_contdemo : MonoBehaviour
             transform.position -= speed * transform.right * Time.deltaTime;
         }
 
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "sencer")
+        {
+            pos = this.transform.position;
+
+            Debug.Log("hit");
+
+            int x = Random.Range(1, 7);
+            int num = x;
+
+            switch (num)
+            {
+                case 1:
+                    Instantiate(stage1, new Vector3(pos.x + 30f, 5, 0.0f), Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(stage2, new Vector3(pos.x + 30f, 5.0f, 0.0f), Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(stage3, new Vector3(pos.x + 30f, 5.0f, 0.0f), Quaternion.identity);
+                    break;
+                case 4:
+                    Instantiate(stage4, new Vector3(pos.x + 30f, 5.0f, 0.0f), Quaternion.identity);
+                    break;
+                case 5:
+                    Instantiate(stage5, new Vector3(pos.x + 30f, 5.0f, 0.0f), Quaternion.identity);
+                    break;
+                case 6:
+                    Instantiate(stage6, new Vector3(pos.x + 30f, 5.0f, 0.0f), Quaternion.identity);
+                    break;
+
+            }
+
+        }
     }
 }
